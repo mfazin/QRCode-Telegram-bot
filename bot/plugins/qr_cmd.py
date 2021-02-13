f04b4dd67c3429b2b256f291e6ceb4d22aeb064b
mfazin <78682897+mfazin@users.noreply.github.com>
2021-02-13 17:11:50 +0530

    Update qr_cmd.py

--- bot/plugins/qr_cmd.py
+++ bot/plugins/qr_cmd.py
@@ -5,7 +5,7 @@ from messages import Msg
 async def start_message(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Hey {message.from_user.first_name},{Msg.start}{Msg.source}",
+        text=f"Hey {message.from_user.first_name},{Msg.start} Please Join My Update Channel @ekbotz_update",
         reply_to_message_id=message.message_id,
     )
 
@@ -13,6 +13,6 @@ async def start_message(client, message):
 async def help_message(client, message):
     await client.send_message(  
         chat_id=message.chat.id,
-        text=f"Hey {message.from_user.first_name},{Msg.help}{Msg.source}",
+        text=f"Hey {message.from_user.first_name},{Msg.help}. Thanks for using me",
         reply_to_message_id=message.message_id,
     )
